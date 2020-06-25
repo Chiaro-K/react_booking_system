dc7ea698380a18ca0804ea264e8ce859a80c4eed
Khanyiso Revu <khanyiso@guestrevu.com>
2020-06-25 22:50:38 +0200

    add background image with transitions and move nav css to App.js file to make it global

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Fragment } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/home/home';
 
--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import Nav from '../../components/nav/nav';
 import './home.scss';
-import Button from '@material-ui/core/Button';
 import { withRouter } from 'react-router-dom';
 
 class Home extends Component {
@@ -10,9 +9,32 @@ class Home extends Component {
     }
     render() {
         return (
-            <div className="HomePage">
+            <main>
                 <Nav />
-            </div>
+                <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner" role="listbox">
+                        <div class="carousel-item active" style={{ backgroundImage: "url('https://source.unsplash.com/LAaSoL0LrYs/1920x1080')" }}>
+                        </div>
+                        <div class="carousel-item" style={{ backgroundImage: "url('https://source.unsplash.com/bF2vsubyHcQ/1920x1080')" }}>
+                        </div>
+                        <div class="carousel-item" style={{ backgroundImage: "url('https://source.unsplash.com/szFUQoyvrxM/1920x1080')" }}>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </main>
         );
     }
 }
