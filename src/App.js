1151d420a176b47fab8c5e1e9697e1953085f93f
Khanyiso Revu <khanyiso@guestrevu.com>
2020-06-26 01:24:58 +0200

    add material icons for body nav

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Fragment } from 'react';
-import logo from './logo.svg';
 import './App.scss';
 import { BrowserRouter, Route, Switch, Redirect } from 'react-router-dom';
 import Home from './pages/home/home';
--- src/components/body-nav/body-nav.js
+++ src/components/body-nav/body-nav.js
@@ -1,6 +1,12 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
 import './body-nav.scss';
+import HomeIcon from '@material-ui/icons/Home';
+import FlightIcon from '@material-ui/icons/Flight';
+import ApartmentIcon from '@material-ui/icons/Apartment';
+import BeachAccessIcon from '@material-ui/icons/BeachAccess';
+import CreateIcon from '@material-ui/icons/Create';
+import PhoneIcon from '@material-ui/icons/Phone';
 
 const BodyNav = () => {
     return (
@@ -13,22 +19,22 @@ const BodyNav = () => {
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ml-auto">
                         <li class="nav-item active">
-                            <NavLink to="/home" className="nav-link">HOME</NavLink>
+                            <NavLink to="/home" className="nav-link"><span><HomeIcon/></span>HOME</NavLink>
                         </li>
                         <li class="nav-item">
-                            <NavLink to="/home" className="nav-link">FLIGHTS</NavLink>
+                            <NavLink to="/home" className="nav-link"><span><FlightIcon/></span>FLIGHTS</NavLink>
                         </li>
                         <li class="nav-item">
-                            <NavLink to="/home" className="nav-link">HOTELS</NavLink>
+                            <NavLink to="/home" className="nav-link"><span><ApartmentIcon/></span>HOTELS</NavLink>
                         </li>
                         <li class="nav-item">
-                            <NavLink to="/home" className="nav-link">HOLIDAYS</NavLink>
+                            <NavLink to="/home" className="nav-link"><span><BeachAccessIcon/></span>HOLIDAYS</NavLink>
                         </li>
                         <li class="nav-item">
-                            <NavLink to="/home" className="nav-link">BLOG</NavLink>
+                            <NavLink to="/home" className="nav-link"><span><CreateIcon/></span>BLOG</NavLink>
                         </li>
                         <li class="nav-item">
-                            <NavLink to="/home" className="nav-link" style={{ marginRight: '0pt' }}>1800 105 2541</NavLink>
+                            <NavLink to="/home" className="nav-link" style={{ marginRight: '0pt' }}><span><PhoneIcon/></span>1800 105 2541</NavLink>
                         </li>
                     </ul>
                 </div>
