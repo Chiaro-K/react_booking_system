868e85dfc5400fca53e399ac1b859ccf6e20e96e
Khanyiso Revu <khanyiso@guestrevu.com>
2020-06-25 21:06:01 +0200

    fix nav appearance

--- src/components/nav/nav.js
+++ src/components/nav/nav.js
@@ -4,26 +4,24 @@ import './nav.scss';
 
 const Nav = () => {
     return (
-        <nav class="navbar navbar-expand-lg navbar-light bg-light fixed-top">
-            <div class="container">
-                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav ml-auto">
-                        <li class="nav-item active">
-                            <NavLink to="/home">HOME</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/home">FAQS</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/home">CONTACT</NavLink>
-                        </li>
-                        <li class="nav-item">
-                            <NavLink to="/home">USD</NavLink>
-                        </li>
-                    </ul>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <NavLink to="/home" className="nav-item nav-link active">HOME</NavLink>
+                    <NavLink to="#" className="nav-item nav-link">FAQS</NavLink>
+                    <NavLink to="#" className="nav-item nav-link">CONTACT</NavLink>
+                    <NavLink to="#" className="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">USD</a>
+                        <div class="dropdown-menu dropdown-primary" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item" href="#">Option 2</a>
+                            <a class="dropdown-item" href="#">Option 3</a>
+                            <a class="dropdown-item" href="#">Option 4</a>
+                        </div>
+                    </NavLink>
                 </div>
             </div>
         </nav>
